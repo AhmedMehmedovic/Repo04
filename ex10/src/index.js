6a9567163b7445fa1113935a80f694834e37a1d4
AhmedMehmedovic <ahmedm.bjelave@gmail.com>
2021-09-17 00:55:35 +0200

    Repo04

---
+++ ex10/src/index.js
@@ -0,0 +1,23 @@
+function main(){    
+
+    // only change code below this line
+     
+     var myFirstName = "Ahmed";
+     var myLastName  = "Mehmedović";
+     var myHomeTown  = "Srebrenik";
+
+    // only change code below this line
+
+
+return {
+    myFirstName,
+    myLastName,
+    myHomeTown
+};
+
+}
+
+
+console.log(main());
+
+module.exports = main;
